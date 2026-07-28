ea80732c7140fa6440d1c460655275279c894a3e
Raj2002-11 <Rajdwivedi11022002@gmail.com>
2026-07-29 00:08:18 +0530

    Get APIs

--- src/app.js
+++ src/app.js
@@ -3,24 +3,57 @@ const connectDB = require("./config/database")
 const app = express();
 const User = require("./model/user");
 
+app.use(express.json());  // handing middleware
 
 app.post("/signup",async (req,res)=>{
-    
-    //creating a new instance of the user model
-    const user = new User({
-        firstName: "Virat",
-        lastName: "Kholi",
-        emailId:"Vira@gmail.com",
-        password:"Virat123"
-    });
 
-    try{await user.save();
+    // creating a new instance of the user model
+    const user = new User(req.body);
+
+    try{
+        await user.save();
     res.send("User Added successfully")}
     catch(err){
         res.status(400).send("Error saving the user " + err.message);
     }
+
+
 });
 
+
+//get user by email
+app.get("/user",async (req,res)=>{
+    const UserEmail =req.body.emailId; 
+
+    try{
+
+        const user = await User.findOne({emailId: UserEmail});
+        res.send(user);
+        // const users = await User.find({emailId: UserEmail});
+        // if(users.length === 0){
+        //     res.status(404).send("User not found")
+        // }
+        // else{
+        //     res.send(users)
+        // }
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+ 
+})
+
+//get user feed 
+app.get("/feed", async (req,res)=>{
+
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(404).send("Something")
+    }
+})
 
 connectDB()
 .then(()=>{
